7460c5fa06aaeff4797bf1015f7dd9924a9b184b
sivaratrisrinivas <srinivassivaratri1122@gmail.com>
2025-10-16 00:35:19 +0530

    Enhance search functionality to be case-insensitive
    - Modify the `search_command` to convert movie titles to lowercase before checking for the query
    - This improvement allows for more flexible search results regardless of the case used in the query

--- cli/lib/keyword_search.py
+++ cli/lib/keyword_search.py
@@ -8,7 +8,7 @@ def search_command(query: str, limit: int = DEFAULT_SEARCH_LIMIT) -> list[dict]:
     movies = load_movies()
     results = []
     for movie in movies:
-        if query in movie["title"]:
+        if query in movie["title"].lower():
             results.append(movie)
             if len(results) >= limit:
                 break
