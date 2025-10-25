b207f6ce4d6af6037d2732fd84f1b47221ba01ed
sivaratrisrinivas <srinivassivaratri1122@gmail.com>
2025-10-26 02:49:36 +0530

    Add embed_text command to CLI and implement embedding generation
    - Introduce a new "embed_text" command in the CLI to allow users to generate embeddings for input text.
    - Refactor the `SemanticSearch` class to include a method for generating embeddings, ensuring proper error handling for empty input.
    - Update the CLI to handle the new command, enhancing the functionality for semantic search capabilities.

--- cli/lib/semantic_search.py
+++ cli/lib/semantic_search.py
@@ -3,7 +3,23 @@ from sentence_transformers import SentenceTransformer
 
 class SemanticSearch:
     def __init__(self, model_name = "all-MiniLM-L6-v2"):
-        self.model = SentenceTransformer(model_name)
+        self.model = SentenceTransformer(model_name, device="cpu")
+    
+    def generate_embedding(self, text: str) -> list[float]:
+        if text is None or text.strip() == "":
+            raise ValueError("Text cannot be empty")
+        embedding = self.model.encode([text])
+        return embedding[0]
+
+
+def embed_text(text: str):
+    semantic_search = SemanticSearch()
+    embedding = semantic_search.generate_embedding(text)
+    print(f"Text: {text}")
+    # for i, dim in enumerate(embedding[:3]):
+    print(f"Embedding: {embedding[:3]}")
+    print(f"Dimensions: {embedding.shape[0]}")
+
 
 
 def verify_model():
--- cli/semantic_search_cli.py
+++ cli/semantic_search_cli.py
@@ -2,7 +2,7 @@
 
 import argparse
 
-from lib.semantic_search import verify_model
+from lib.semantic_search import embed_text, verify_model
 
 def main():
     parser = argparse.ArgumentParser(description="Semantic Search CLI")
@@ -10,11 +10,16 @@ def main():
 
     subparsers.add_parser("verify", help="Verify that the embedding model is loaded")
     
+    embed_text_parser = subparsers.add_parser("embed_text", help="Embed a text")
+    embed_text_parser.add_argument("text", type=str, help="Text to embed")
+    
     args = parser.parse_args()
 
     match args.command:
         case "verify":
             verify_model()
+        case "embed_text":
+            embed_text(args.text)
         case _:
             parser.print_help()
 
